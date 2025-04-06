7cc70c89f94ae6a625375944618d3d33ca1ef808
GuillermoBarreto <Guilleelias25@gmail.com>
2025-04-06 15:33:27 -0400

    creating print for the functions

--- Calculator.java
+++ Calculator.java
@@ -7,5 +7,13 @@ public class Calculator {
         char operator;
 
         System.out.println("My personal calculator");
+
+        System.out.println("Enter first number:");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, ./): ");
+        operator = scanner.next().chartAt(0);
+
+        System.out.print("")
     }
 }
