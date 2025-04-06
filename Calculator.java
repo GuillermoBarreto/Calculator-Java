249b06188e461c75ab60523dc1f39aff4513d2d4
GuillermoBarreto <Guilleelias25@gmail.com>
2025-04-06 15:29:50 -0400

    project

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main (String)
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double num1, num2, result = 0
+        char operator;
+
+        System.out.println("My personal calculator");
+    }
 }
